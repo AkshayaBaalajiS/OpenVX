f3eb09beedc03f4ce1cd2c2615a02eaaf963e8d3
Akshaya Baalaji S <baalaji12akshay@gmail.com>
2026-03-08 18:22:34 +0000

    vx_graph_2 has the code to create the data objects and how the objects are created

---
+++ vx_graph/vx_graph_2/vx_graph.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <VX/vx.h>
+using namespace std;
+
+int main()
+{
+	cout<<"This is the code to understand the vx_graph in OpenVX\n";
+
+	vx_status stat = VX_SUCCESS;
+
+	// context creation 
+	// vx_context cont = vxCreateContext() ; // this is initialization of the context 
+	vx_context cont;
+	// we can also assign a context 
+	cont = vxCreateContext();
+
+	if(cont == nullptr)
+	{	
+		cout<<"[DEBUG PRINT] vxCreateContext failed\n";
+		return -1;
+	}
+	
+	if(cont != nullptr)
+	{	
+		cout<<"[DEBUG PRINT] vxCreateContext success..\n";
+	}
+	
+	
+
+	// graph creation 
+	vx_graph gra = vxCreateGraph(cont);
+
+	if(gra == nullptr)
+	{	
+		cout<<"[DEBUG PRINT] vxCreateGraph failed\n";
+		return -1;
+	}
+	// if(gra == VX_SUCCESS) // this cant be done 
+	if(gra != nullptr)
+	{	
+		cout<<"[DEBUG PRINT] vxCreateGraph success\n";
+	}
+
+	// data object 
+	vx_image inputImg = vxCreateImage(cont, 640, 640, VX_DF_IMAGE_U8);
+	vx_image OpImg = vxCreateImage(cont, 640, 640, VX_DF_IMAGE_U8);
+	if(inputImg != nullptr || OpImg != nullptr )
+	{
+		cout<<"[DEBUG PRINT] vxCreateImage success for input and output \n";
+	}
+	else
+	{
+		cout<<"[DEBIG PRINT] vxCreateImage failed for input and output \n";
+	}
+
+
+	// node creation 
+	// vx_node nodeobj = vxCreateNode(gra, inputImg, OpImg);
+
+
+
+	// verify graph
+	stat = vxVerifyGraph(gra);
+	if(stat == VX_SUCCESS)
+	{
+		cout<<"[DEBUG PRINT] vxVerifyGraph success\n";
+	}
+	else
+	{
+		cout<<"[DEBUG PRINT] vxVerifyGraph failed\n";
+		return -1;	
+	}	
+
+	// process graph 
+	stat = vxProcessGraph(gra);
+	if(stat == VX_SUCCESS)
+	{
+		cout<<"[DEBUG PRINT] vxProcessGraph success\n";
+	}
+	else{
+		cout<<"[DEBUG PRINT] vxProcessGraph failed\n";
+	}
+	
+
+	vxReleaseGraph(&gra);
+
+	vxReleaseContext(&cont);
+}
