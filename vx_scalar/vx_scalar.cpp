4649932388c75d1ccaedce760cbf81d2dc660bc0
Akshaya Baalaji S <baalaji12akshay@gmail.com>
2026-03-12 04:56:54 +0000

    scalar creation in OpenVX

---
+++ vx_scalar/vx_scalar.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <VX/vx.h>
+
+using namespace std;
+
+int main()
+{
+	cout<<"vx_Scalar creation in OpenVX\n";
+	vx_context cont = vxCreateContext();
+
+	int number = 123;
+	vx_scalar sca = vxCreateScalar(cont, VX_TYPE_INT32, &number);
+
+	cout<<"[DEBUG PRINT] sca = " << sca <<endl;
+
+	vxReleaseContext(&cont);
+}
